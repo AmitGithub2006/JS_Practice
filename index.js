759b8be0feac43e0aca3d1c133031ec32cb4f763
Amit Kumar <shakya94ak@gmail.com>
2022-06-25 16:07:29 +0530

    class ques

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
-
-//Print the numbers between the given range using recursion.
-// function x(a,b){
-//     if(a<b){
-//         console.log(a);
-//         return x(a+1,b);
-//     }
-//     return a;
-// }
-// console.log(x(11,20));
-
-//Given a number n,print numbers from 1 to n using recursion.
-// function x(n1,n2) {
-// if(n1<=n2){
-//     console.log(n1);
-//     return x(n1+1,n2);
-// }
-// }
-// x(11,20);
-
-//Print all the numbers in descending order in the given range using recursion.
-//  function x(a,b) {
-//     if(a>=b){
-// console.log(a);
-// return x(a-1,b);
-//     }
-//  }
-//  x(20,10);
-
-
- //Print factorial of a number using recursion.
+//1.Print factorial of a number using recursion.
 //  function facto(num,fact){
 //     if(num>0){
 //         fact *= num;
@@ -38,17 +8,36 @@
 //  }
 //  console.log(facto(5,1))
 
-//Given a number n, print all the even numbers from n to 1 using recursion.
-function number(n,num2){
-    if(n%2 == 0){
-        // console.log(n);
-        return number(n-1,num2);
-    }
-    return num2
-}
-console.log(number(10,1));
+//2.Given a number n, print numbers from 1 to n using recursion.
+// function x(a,n){
+//     if(a<n){
+//         console.log(a);
+//         return x(a+1,n);
+//     }
+//     return a;
+// }
+// console.log(x(1,20));
 
-//Write a Javascript program to count the number of digits a given number has using recursion.
+//3.Given a number n,print numbers from n to 1 using recursion.
+// function x(n1,n2) {
+// if(n1<=n2){
+//     console.log(n2);
+//     return x(n1,n2-1);
+// }
+// }
+// x(1,20);
+
+//4.Print ‘n’ numbers of Fibonacci series using recursion.
+// function fibo(a, b, counter) {
+//   if (counter <= 12) {
+//     let c = a + b;
+//     console.log(a);
+//     return fibo(b, c, counter + 1);
+//   }
+// }
+// fibo(0, 1, 1);
+
+//6.Write a Javascript program to count the number of digits a given number has using recursion.
 // function numberD(number1,count){
 //     if(number1>0){
 //         number1 = parseInt(number1/10);
@@ -58,3 +47,35 @@ console.log(number(10,1));
 //     return count
 // }
 // console.log(numberD(5456765,0))
+
+//9.Given a number n, print all the odd numbers from 1 to n using recursion.
+// function num(a, b) {
+//   if (a <= b) {
+//       console.log(a);
+//       return num(a + 2, b);
+//   }
+// }
+// num(1, 10);
+
+//10.Given a number n, print all the even numbers from n to 1 using recursion.
+// function number(n, num2) {
+//   if (n <= num2) {
+//     console.log(num2);
+//     return number(n, num2 - 2);
+//   }
+//   return num2;
+// }
+// number(1, 10);
+
+//11.Write a javascript program to check whether the given string is palindrome or not using recursion.
+
+//12.Given a number n, find 2^n using recursion.
+function num(a, n) {
+  if(n==0){
+    return 1;
+  }
+  else{
+    return (a* num(a,n-1));
+  }
+}
+console.log(num(2, 4));
---
+++ readme.md
@@ -0,0 +1,8 @@
+<img src= "ques1.png">
+<img src= "ques2.png">
+<img src= "ques3.png">
+<img src= "ques4.png">
+<img src= "ques6.png">
+<img src= "ques9.png">
+<img src= "ques10.png">
+<img src= "ques12.png">
